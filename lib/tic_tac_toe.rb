035f367de1a864330cc1c2f9b8522ae3dbfa624b
Dakota States <dakota.states@gmail.com>
2020-02-22 05:24:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,6 +39,16 @@ WIN_COMBINATIONS = [
     index.between?(0,8) && !position_taken?(index)
   end
   
+  def turn_count
+    number_of_turns = 0
+    @board.each do |space|
+      if ((space == "X") || (space == "O"))
+          number_of_turns += 1
+      end
+    end
+    number_of_turns
+  end
+  
   def turn
     puts "Please enter 1-9:"
     input = gets
@@ -52,14 +62,7 @@ WIN_COMBINATIONS = [
     end
   end
   
-  def turn_count
-    number_of_turns = 0
-    @board.each do |space|
-    if ((space == "X") || (space == "O"))
-        number_of_turns += 1
-    end
-    number_of_turns
-  end
+
   
   def current_player
     if turn_count() % 2 != 0
